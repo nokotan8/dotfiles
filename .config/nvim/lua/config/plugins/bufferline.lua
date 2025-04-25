90751fb118dc8b96820df66f320fb2e11a884596
Justin Tao <z5476270@ad.unsw.edu.au>
2025-04-25 15:39:34 +1000

    added bufferline

---
+++ .config/nvim/lua/config/plugins/bufferline.lua
@@ -0,0 +1,18 @@
+return {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+        local bufferline = require('bufferline')
+        bufferline.setup({
+            options = {
+                buffer_close_icon = ' ',
+                close_icon = ' ',
+                style_preset = {
+                    bufferline.style_preset.no_italic,
+                    bufferline.style_preset.minimal,
+                }
+            },
+        })
+    end
+}

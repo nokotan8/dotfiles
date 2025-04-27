7634b1c80174c197fdb1a60577cdec73346a73d0
Justin Tao <z5476270@ad.unsw.edu.au>
2025-04-27 15:54:50 +1000

    replaced oil with yazi

--- .config/nvim/lua/justint/plugins/oil.lua
+++ .config/nvim/lua/justint/plugins/oil.lua
@@ -1,14 +1,14 @@
 return {
-    'stevearc/oil.nvim',
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-
-    config = function()
-        require("oil").setup({
-            view_options = {
-                show_hidden = true,
-            }
-
-        })
-        vim.keymap.set({ "n", "x" }, "<leader>o", function() require("oil").open() end, { silent = true })
-    end
+    -- 'stevearc/oil.nvim',
+    -- dependencies = { "nvim-tree/nvim-web-devicons" },
+    --
+    -- config = function()
+    --     require("oil").setup({
+    --         view_options = {
+    --             show_hidden = true,
+    --         }
+    --
+    --     })
+    --     vim.keymap.set({ "n", "x" }, "<leader>o", function() require("oil").open() end, { silent = true })
+    -- end
 }

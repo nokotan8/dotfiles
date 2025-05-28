ac830d0971f6fded0006e125402245cda211b3ec
fancywhale23 <justintaoau@gmail.com>
2025-05-28 20:20:43 +1000

    added function to replace vector|string with std::vector|string

--- .config/nvim/lua/justint/keys.lua
+++ .config/nvim/lua/justint/keys.lua
@@ -3,18 +3,16 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>q", ":q<CR>")
 vim.keymap.set("n", "<leader>w", ":wa<CR>", { silent = true })
 
--- move lines
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p')
+vim.keymap.set({ "n", "v" }, "<leader>P", '"+P')
 -- vim.keymap.set({ "n", "v" }, "<leader>d", '"+d')
 
 -- paste/delete to void
-vim.keymap.set("x", "<leader>P", '\"_dP')
 vim.keymap.set({ "n", "v" }, "<leader>d", '\"_d')
+-- vim.keymap.set("x", "<leader>P", '\"_dP')
 
 -- buffer nav
 -- vim.keymap.set('n', '<leader>[', ':bp<CR>', { noremap = true, silent = true })
@@ -28,34 +26,49 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+-- move lines
+-- I basically never use this
+-- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+-- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- vim.keymap.set("i", "<C-c>", "<Esc>")
 
-local function tree()
-    local node = vim.treesitter.get_node()
-    local i = 0
-    while true do
-        print(node:type())
-        if (node:type() ~= "code") then
-            node = node:parent()
-        else
-            break
-        end
+-- Function to replace "vector|string" with "std::vector|string" and add the #include
+-- For leetcode LMAO
+function CppAddStd()
+    local found1 = false
+    local found2 = false;
+    local line_count = vim.api.nvim_buf_line_count(0)
 
-        if i < 10 then
-            break
+    for i = 0, line_count - 1 do
+        local line = vim.api.nvim_buf_get_lines(0, i, i + 1, false)[1]
+        if string.find(line, "vector") then
+            found1 = true
+            local new_line = string.gsub(line, "vector", "std::vector")
+            vim.api.nvim_buf_set_lines(0, i, i + 1, false, { new_line })
         end
-        i = i + 1
+        if string.find(line, "string") then
+            found2 = true
+            local new_line = string.gsub(line, "string", "std::string")
+            vim.api.nvim_buf_set_lines(0, i, i + 1, false, { new_line })
+        end
     end
 
-    local start_row, start_col, end_row, end_col = node:range()
-
-    vim.api.nvim_win_set_cursor(0, { start_row + 1, start_col - 1 })
-    vim.cmd('normal! v') -- start visual mode
-    vim.api.nvim_win_set_cursor(0, { end_row + 1, end_col - 1 })
+    if found1 then
+        local lines = vim.api.nvim_buf_get_lines(0, 0, 1, false)
+        if lines[1] ~= "#include <vector>" then
+            vim.api.nvim_buf_set_lines(0, 0, 0, false, { "#include <vector>" })
+        end
+    end
+    if found2 then
+        local lines = vim.api.nvim_buf_get_lines(0, 0, 1, false)
+        if lines[1] ~= "#include <string>" then
+            vim.api.nvim_buf_set_lines(0, 0, 0, false, { "#include <string>" })
+        end
+    end
 end
 
+vim.keymap.set("n", "<leader>lc", CppAddStd, { noremap = true, silent = true })
 
-vim.keymap.set({ "n", "v" }, "<leader>o", tree, {})
 -- vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 -- vim.keymap.set('n', '<leader>k', [[:split | terminal
 -- A]], { desc = "Exit terminal mode" })

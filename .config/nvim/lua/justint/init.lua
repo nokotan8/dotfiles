996f8fc731642ca833509c255cf521b8f1d46b4e
fancywhale23 <justintaoau@gmail.com>
2025-06-09 18:58:42 +1000

    update nvim to recognise .cl as .c

--- .config/nvim/lua/justint/init.lua
+++ .config/nvim/lua/justint/init.lua
@@ -1,3 +1,8 @@
 require "justint.opts"
 require "justint.keys"
 require "justint.lazy"
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = "*.cl",
+    command = "set filetype=c"
+})
